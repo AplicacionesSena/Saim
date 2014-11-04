06f976f58f206a8e8e44dd40b2b9ff15e82f8fb7
Juanse Rios <juan147123@gmail.com>
2014-11-04 15:12:49 -0500

    vistas y diseño

--- db/migrate/20141030203205_create_elementos.rb
+++ db/migrate/20141030203205_create_elementos.rb
@@ -1,14 +1,14 @@
 class CreateElementos < ActiveRecord::Migration
   def change
     create_table :elementos do |t|
-      t.integer :placa
+      t.string :placa
       t.references :tipo_elem, index: true
       t.string :nombre
       t.text :descripcion
       t.string :modelo
       t.string :serial
       t.datetime :fecha_adquisicion
-      t.integer :valor
+      t.string :valor
       t.boolean :estado
       t.references :cuentadante, index: true
 
